82c35c8ac0d09b9b89d17d101a791fd7f8fc5e38
s.brescia <s.brescia@grafspa.it>
2021-04-29 09:41:02 +0200

    Android App

--- GoogleAssistantProject/app/src/main/java/com/example/googleassistantproject/MainActivity.java
+++ GoogleAssistantProject/app/src/main/java/com/example/googleassistantproject/MainActivity.java
@@ -112,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void UpdateDev() throws InterruptedException {
-        Thread.sleep(1500);
+        Thread.sleep(2000);
         deviceArrayAdapter.clear();
         toDb.getDocument(deviceArrayAdapter);
     }
